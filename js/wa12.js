47f86d0e76dcea9382e80b0608e6001c118445b2
SarahStonehocker <sast7628@colorado.edu>
2024-11-18 13:41:32 -0700

    upload

---
+++ js/wa12.js
@@ -0,0 +1,24 @@
+let myData =  {};
+
+function fetchData(){
+fetch('https://xkcd.com/info.0.json')
+    .then(res => {
+        if(res.ok){
+            return res.json();
+            
+        }else{
+            console.log(res);
+        }
+        
+    }) .then(res => {
+        myData = res;
+        console.log(myData);
+        document.getElementById("fact").innerHTML = myData.fact;
+    })
+    .catch(error => {console.log(error)})
+}
+fetchData();
+
+document.getElementById("generate"). addEventListener("click", e=> {fetchData();})
+
+console.log(myData);
